0bb902989548e1885087443a11c42c8f384382fb
Viktor Spivak <spivakmailbox@gmail.com>
2019-08-23 21:46:13 +0300

    third commit+

---
+++ task-07.js
@@ -0,0 +1,22 @@
+const numbers = [];
+while (true) {
+  let input = prompt("Введите число");
+  if (!Number(input) && input != null) {
+    alert("Вы  ввели не число");
+    continue;
+  }
+  if (input === null) {
+    if (numbers.length === 0) {
+      console.log("Вы ничего не ввели");
+      break;
+    }
+    let total = 0;
+    for (let number of numbers) {
+      total = total + number;
+    }
+    console.log(`Общая сумма чисел равна ${total}`);
+    break;
+  } else {
+    numbers.push(Number(input));
+  }
+}
